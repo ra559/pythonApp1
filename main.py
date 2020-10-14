9173f361bcc8b45e6dbaa0606ac27e34a13a0385
Robert Alberto <ra559@njit.edu>
2020-10-14 00:20:25 -0400

    Feature: Added calculator

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
-def menu():
-    print("What would you like?")
-    print("1- Burger")
-    print("2- Pizza")
-    print("3- Hot Dog")
+numb1 = int(input("Enter number: "))
+numb2 = int(input("Enter another number: "))
 
-menu()
+def addition(numb1, numb2):
+    result = numb1 + numb2
+    return result
+
+
+def subtraction(numb1, numb2):
+    if numb1 > numb2:
+        bigger = numb1
+        smaller = numb2
+    else:
+        bigger = numb2
+        smaller = numb1
+    result = bigger - smaller
+    return result
+
+
+def multiplacation(numb1, numb2):
+    result = numb1 * numb2
+    return result
+
+
+def division(numb1, numb2):
+    if numb1 > numb2:
+        bigger = numb1
+        smaller = numb2
+    else:
+        bigger = numb2
+        smaller = numb1
+    result = bigger / smaller
+    return result
+
+
+print("The addition of ", numb1, "and", numb2, "is", addition(numb1, numb2))
+print("The subtraction of ", numb1, "and", numb2, "is", subtraction(numb1, numb2))
+print("The multiplication of ", numb1, "and", numb2, "is", multiplacation(numb1, numb2))
+print("The division of ", numb1, "and", numb2, "is", division(numb1, numb2))
